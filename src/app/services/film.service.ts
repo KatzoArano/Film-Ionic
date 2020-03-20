5602485ee4c32c5b85589454801a85c333e0c729
oarano <oarano@macbook-pro-de-oarano.home>
2020-03-20 14:14:15 +0100

    Creation services avec méthodes recherche film et details

--- src/app/services/film.service.ts
+++ src/app/services/film.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {IFilms} from '../model/IFilm.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmService {
+  private url : string = "";
+  private apiKey: string = "a4f05c48";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  searchFilms(title: string, type: string){
+    this.url = `http://www.omdbapi.com/?s=${encodeUri(title)}&type=${type}$apiKey=${this.apiKey}`;
+    return this.http.get<IFilms>(this.url).pipe(map(results => results['Search']));
+  }
+
+  getDetailsFilm(id: string){
+    return this.http.get<IFilms>(`http://www.omdbapi.com/?i=${id}&plot=full$apiKey=${this.apiKey}`)
+  }
 }
